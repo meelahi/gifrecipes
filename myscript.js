0d37f3aa6d44d9b94ef6333c768e2782c79f0507
SPARROW-BASE\meela <meelahi@outlook.com>
2018-04-01 12:45:28 -0400

    Implemented Tabs on desktop

--- myscript.js
+++ myscript.js
@@ -1,2 +1,16 @@
 var color="#2E2E2E";
 var border_space="3px";
+
+function openFood(evt, foodCategory) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(foodCategory).style.display = "block";
+    evt.currentTarget.className += " active";
+}
