2f8288b206b021870648cbdc77f9d22a650e61c6
SPARROW-BASEV2\Manzoor Elahi <melahi@fbbrands.com>
2025-11-16 01:24:21 -0500

    js file formatting updates

--- myscript.js
+++ myscript.js
@@ -1,13 +1,22 @@
+let color = "#2E2E2E";
+let border_space = "3px";
+
 function openFood(evt, foodCategory) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    // Hide all tab content
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    // Remove "active" from all tab buttons
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
     }
+
+    // Show selected category
     document.getElementById(foodCategory).style.display = "block";
-    evt.currentTarget.className += " active";
+
+    // Add active class to clicked button
+    evt.currentTarget.classList.add("active");
 }
